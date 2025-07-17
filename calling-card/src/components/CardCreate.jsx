d864d48695b53a169a36a36eed1f1a7788aabb08
eagleeye1689@gmail.com <eagleeye1689@gmail.com>
2025-07-17 15:40:45 -0500

    Code cleanup

--- calling-card/src/components/CardCreate.jsx
+++ calling-card/src/components/CardCreate.jsx
@@ -7,7 +7,6 @@ const CardCreate = () => {
             <form>
                 <input id="name" type="text" name="name" placeholder="Name" /><br/>
                 <input id="phone" type="text" name="phone" placeholder="Phone Number" /><br/>
-
                 <input id="email" type="text" name="email" placeholder="E-Mail"/><br/>
                 <input id="address1" type="text" name="address1" placeholder="Address Line 1"/><br/>
                 <input id="address2" type="text" name="address2" placeholder="Address Line 2"/><br/>
