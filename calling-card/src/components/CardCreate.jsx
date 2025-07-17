209eb166bc8370e41931c6d2d176d7e2ca4f807f
eagleeye1689@gmail.com <eagleeye1689@gmail.com>
2025-07-17 15:23:05 -0500

    Added phone number line and 2nd address line to create form

--- calling-card/src/components/CardCreate.jsx
+++ calling-card/src/components/CardCreate.jsx
@@ -5,9 +5,12 @@ const CardCreate = () => {
     return(
         <div >
             <form>
-                <input id="name" type="text" name="name" placeholder="Enter Name" /><br/>
-                <input id="email" type="text" name="email" placeholder="Enter E-Mail"/><br/>
-                <input id="address" type="text" name="address" placeholder="Enter Address"/><br/>
+                <input id="name" type="text" name="name" placeholder="Name" /><br/>
+                <input id="phone" type="text" name="phone" placeholder="Phone Number" /><br/>
+
+                <input id="email" type="text" name="email" placeholder="E-Mail"/><br/>
+                <input id="address1" type="text" name="address1" placeholder="Address Line 1"/><br/>
+                <input id="address2" type="text" name="address2" placeholder="Address Line 2"/><br/>
                 <Link to="/preview">
                     <button type="button">View</button>
                 </Link>
