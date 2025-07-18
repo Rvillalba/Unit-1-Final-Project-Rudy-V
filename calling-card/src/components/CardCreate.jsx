efc3e1474c7495352fcba2cd5526d50009eb9979
Rudy V <eagleeye1689@gmail.com>
2025-07-17 23:25:54 -0500

    End of day commit

--- calling-card/src/components/CardCreate.jsx
+++ calling-card/src/components/CardCreate.jsx
@@ -21,23 +21,27 @@ const CardCreate = () => {
     };
     return(
         <div>
-            <h1>Card Fields</h1>            
-                <form>
-                <input id="name" type="text" name="name" value={formData.name} onChange={handleChange} placeholder="Name" /> <br/>
-                <input id="phone" type="text" name="phone" value={formData.phone} onChange={handleChange} placeholder="Phone Number" /> <br/>
-                <input id="email" type="text" name="email" value={formData.email} onChange={handleChange} placeholder="E-Mail" /> <br/>
-                <input id="address1" type="text" name="address1" value={formData.address1} onChange={handleChange} placeholder="Address Line 1" /> <br/>
-                <input id="address2" type="text" name="address2" value={formData.address2} onChange={handleChange} placeholder="Address Line 2" /> <br/>
-                <Link to="/preview">
-                    <button type="button">View</button>
-                </Link>
-                </form>
-            <h2>Preview</h2>
-            <p>Name: {formData.name}</p>
-            <p>Phone: {formData.phone}</p>
-            <p>E-mail: {formData.email}</p>
-            <p>Address Line 1: {formData.address1}</p>
-            <p>Address Line 2: {formData.address2}</p>
+            <div>
+                <h1>Card Fields</h1>            
+                    <form>
+                    <input id="name" type="text" name="name" value={formData.name} onChange={handleChange} placeholder="Name" /> <br/>
+                    <input id="phone" type="text" name="phone" value={formData.phone} onChange={handleChange} placeholder="Phone Number" /> <br/>
+                    <input id="email" type="text" name="email" value={formData.email} onChange={handleChange} placeholder="E-Mail" /> <br/>
+                    <input id="address1" type="text" name="address1" value={formData.address1} onChange={handleChange} placeholder="Address Line 1" /> <br/>
+                    <input id="address2" type="text" name="address2" value={formData.address2} onChange={handleChange} placeholder="Address Line 2" /> <br/>
+                    <Link to="/preview">
+                        <button type="button">View</button>
+                    </Link>
+                    </form>
+            </div>
+            <div>
+                <h2>Preview</h2>
+                <p>{formData.name}</p>
+                <p>{formData.phone}</p>
+                <p>{formData.email}</p>
+                <p>{formData.address1}</p>
+                <p>{formData.address2}</p>
+            </div>
         </div>
     )
 }
