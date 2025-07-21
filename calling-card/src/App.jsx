1f3922600acfb8f25bab6c354a6426509a6c8ec0
eagleeye1689@gmail.com <eagleeye1689@gmail.com>
2025-07-21 12:45:16 -0500

    Removed unused link

--- calling-card/src/App.jsx
+++ calling-card/src/App.jsx
@@ -1,9 +1,6 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Landing from './components/Landing';
 import CardCreate from './components/CardCreate';
-import Saved from './components/SavedCards';
-import Preview from './components/Preview';
-import Register from './components/Register';
 import './App.css';
 
 function App() {
@@ -13,9 +10,6 @@ function App() {
                 <Routes>
                         <Route path="/" element={<Landing />} />
                         <Route path="/create" element={<CardCreate />} />
-                        <Route path="/saved" element={<Saved />} />
-                        <Route path="/preview" element={<Preview />} />
-                        <Route path="/register" element={<Register />} />
                 </Routes>
             </Router>
         </div>
