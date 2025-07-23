8b56bce47b197f36c7961e1896398bf723dc7879
eagleeye1689@gmail.com <eagleeye1689@gmail.com>
2025-07-23 12:33:31 -0500

    changed the download feature to html2canvas

--- calling-card/src/components/CardCreate.jsx
+++ calling-card/src/components/CardCreate.jsx
@@ -1,7 +1,8 @@
 import { useParams } from "react-router-dom";
 import { useState } from "react";
-import CardPreview from './CardPreview';
+import CardPreview from "./CardPreview";
 import Button from "./Button";
+
 const CardCreate = () => {
     const { create }=useParams();
     const [formData, setFormData] = useState({
@@ -27,14 +28,14 @@ const CardCreate = () => {
         address2:"",
         });
     };
-        return(       
+    return(       
         <div>
-            <div class="create-form">
+            <div className="create-form">
                 <h1 className="create-title">Enter Information Below</h1>            
                     <form>
                         <input id="name" type="text" name="name" value={formData.name} onChange={handleChange} placeholder="Name" /> <br/>
                         <input id="phone" type="tel" name="phone" value={formData.phone} onChange={handleChange} placeholder="Phone Number" /> <br/>
-                        <input id="email" type="text" name="email" value={formData.email} onChange={handleChange} placeholder="E-Mail" /> <br/>
+                        <input id="email" type="email" name="email" value={formData.email} onChange={handleChange} placeholder="E-Mail" /> <br/>
                         <input id="address1" type="text" name="address1" value={formData.address1} onChange={handleChange} placeholder="Address Line 1" /> <br/>
                         <input id="address2" type="text" name="address2" value={formData.address2} onChange={handleChange} placeholder="Address Line 2" /> <br/>
                     <Button label="Clear All Fields" onClick={clearInput} />
--- calling-card/src/components/CardPreview.jsx
+++ calling-card/src/components/CardPreview.jsx
@@ -1,52 +1,45 @@
-import { toPng } from 'html-to-image'
+import html2canvas from 'html2canvas';
 import { useRef } from "react";
-import { useParams } from "react-router-dom";
 import Button from './Button';
-import backgroundImage from '../images/bg-image.jpg'
-
+import cardBackground from '../images/bg-image.jpg'
 
 const CardPreview = ({ data }) => {
-    const { preview }=useParams();
     const elementRef = useRef(null);
-    const htmlToImageConvert = () => {
-    toPng(elementRef.current, { cacheBust: false })
-      .then((dataUrl) => {
-        const link = document.createElement("a");
-        link.download = "calling-card.png";
-        link.href = dataUrl;
-        link.click();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-    return(
-        <div>
+    const htmlToCanvas = () => {
+      if(!elementRef.current) return;
 
-<div className="business-card"
-                ref={elementRef}
-                style={{
-                    backgroundImage: `url(${backgroundImage})`,
-                    width: "325px",
-                    height: "auto",
-                    backgroundColor: "#fff",
-                    padding: "30px",
-                    boxShadow: "0px 0px 20px rgba(0, 0, 0, 0.2)",
-                    borderRadius: "10px",
-                    margin: "40px auto",
-                    fontFamily: "Arial, Helvetica, sans-serif",
-                    textAlign: "left",
-                    marginBottom: "20px",
-                    color: "#333",
-                }}>
-                <p>{data.name}</p>
-                <p>{data.phone}</p>
-                <p>{data.email}</p>
-                <p>{data.address1}</p>
-                <p>{data.address2}</p><br/>
-                
-            </div>
-            <Button label="Download Card" onClick={htmlToImageConvert} />
+      html2canvas(elementRef.current).then(canvas => {
+        const dataUrl = canvas.toDataURL('image/png');
+        const link = document.createElement("a");
+        link.href = dataUrl;
+        link.download = "calling-card.png";
+        link.click();
+      });
+     };
+    return(
+      <div>
+        <div 
+          className="business-card"
+          ref={elementRef}
+          style={{
+            backgroundImage: `url(${cardBackground})`,
+            width: 325,
+            minHeight: 200,
+            backgroundColor: '#fff',
+            color: '#000',
+            padding: 20,
+            fontFamily: 'Arial, Helvetica, sans-serif',
+            textAlign: 'left',
+            margin: '40px auto',
+        }}
+        >
+            <p>{data.name}</p>
+            <p>{data.phone}</p>
+            <p>{data.email}</p>
+            <p>{data.address1}</p>
+            <p>{data.address2}</p><br/>
+        </div>
+            <Button label="Download Card" onClick={htmlToCanvas} />
         </div>
     )
 }
