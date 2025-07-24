aba21c99ef38fdb32234b678cdffd0ef4f7b835e
Rudy <eagleeye1689@gmail.com>
2025-07-23 21:13:51 -0500

    Added css for about page

--- calling-card/src/components/About.jsx
+++ calling-card/src/components/About.jsx
@@ -4,8 +4,8 @@ const About = () => {
     const { about }=useParams();
 
     return(
-        <div>
-            <h1>How I came into existence</h1>
+        <div id="about">
+            <h1>How Calling Card came into existence</h1>
             <p>Calling card first crept into my mind when I went to a networking event. When I realized that I had not brought any of my business cards
                 to the event, I wished there was an app to generate a quick business card so I could give it to anyone that might want my contact information. <br/><br/>
 
@@ -13,7 +13,7 @@ const About = () => {
 
                 While Calling Card was perfect for my situation, it could be perfect for you! Are you:
             </p>
-            <ul className="about-list">
+            <ul id="about-list">
                 <li>A college student who would rather avoid paying for 100s of physical business cards</li>
                 <li>A parent wanting a quick and easy way to share contact info with fellow parents in your child's classroom</li>
                 <li>A person meeting people at a bar looking for a unique way to share contact information</li>
