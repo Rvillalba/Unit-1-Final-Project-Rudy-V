763619aab3faa8b5562883e336d9537b8d70d7db
Rudy <eagleeye1689@gmail.com>
2025-07-23 22:11:32 -0500

    CSS update

--- calling-card/src/components/About.jsx
+++ calling-card/src/components/About.jsx
@@ -4,21 +4,23 @@ const About = () => {
     const { about }=useParams();
 
     return(
-        <div id="about">
-            <h1>How Calling Card came into existence</h1>
-            <p>Calling card first crept into my mind when I went to a networking event. When I realized that I had not brought any of my business cards
-                to the event, I wished there was an app to generate a quick business card so I could give it to anyone that might want my contact information. <br/><br/>
+        <div>
+            <section id="about">
+                <h1>How Calling Card came into existence</h1>
+                    <p>Calling card first crept into my mind when I went to a networking event. When I realized that I had not brought any of my business cards
+                        to the event, I wished there was an app to generate a quick business card so I could give it to anyone that might want my contact information. <br/><br/>
 
-                Thus, Calling Card was calling. <br/><br/>
+                        Thus, Calling Card was calling. <br/><br/>
 
-                While Calling Card was perfect for my situation, it could be perfect for you! Are you:
-            </p>
-            <ul id="about-list">
-                <li>A college student who would rather avoid paying for 100s of physical business cards</li>
-                <li>A parent wanting a quick and easy way to share contact info with fellow parents in your child's classroom</li>
-                <li>A person meeting people at a bar looking for a unique way to share contact information</li>
-            </ul>
-            <p>If there is any reason you could use a digital contact card, Calling Card is for you!</p>
+                        While Calling Card was perfect for my situation, it could be perfect for you! Are you:
+                    </p>
+                    <ul id="about-list">
+                        <li>A college student who would rather avoid paying for 100s of physical business cards</li>
+                        <li>A parent wanting a quick and easy way to share contact info with fellow parents in your child's classroom</li>
+                        <li>A person meeting people at a bar looking for a unique way to share contact information</li>
+                    </ul>
+                    <p>If there is any reason you could use a digital contact card, Calling Card is for you!</p>
+            </section>
         </div>
     )
 }
