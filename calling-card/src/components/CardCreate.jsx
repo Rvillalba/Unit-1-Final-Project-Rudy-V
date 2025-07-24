9c604ab345f9391286e009f488306df21efa70ee
eagleeye1689@gmail.com <eagleeye1689@gmail.com>
2025-07-24 12:15:21 -0500

    Added another media query to fix issue with preview area cutting off

--- calling-card/src/components/CardCreate.jsx
+++ calling-card/src/components/CardCreate.jsx
@@ -12,9 +12,9 @@ const CardCreate = () => {
             <div id="create-form">
                 <h1 id="create-title">Enter Information Below</h1>            
                     <form id="form">
-                        <input id="name" type="text" name="name" value={formData.name} onChange={handleChange} placeholder="Name" /> <br/>
-                        <input id="phone" type="tel" name="phone" value={formData.phone} onChange={handleChange} placeholder="Phone Number" /> <br/>
-                        <input id="email" type="email" name="email" value={formData.email} onChange={handleChange} placeholder="E-Mail" /> <br/>
+                        <input id="name" type="text" name="name" value={formData.name} onChange={handleChange} required placeholder="Name" /> <br/>
+                        <input id="phone" type="tel" name="phone" value={formData.phone} onChange={handleChange} required placeholder="Phone Number" /> <br/>
+                        <input id="email" type="email" name="email" value={formData.email} onChange={handleChange} required placeholder="E-Mail" /> <br/>
                         <input id="address1" type="text" name="address1" value={formData.address1} onChange={handleChange} placeholder="Address Line 1" /> <br/>
                         <input id="address2" type="text" name="address2" value={formData.address2} onChange={handleChange} placeholder="Address Line 2" /> <br/>
                         <Button id="btn" label="Clear All Fields" onClick={clearInput} />
